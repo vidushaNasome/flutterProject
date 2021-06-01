d0a6143eca386bd53a4993abf1b943521638f976
vidushaNasome <vnasome58@gmail.com>
2021-06-01 22:18:08 +0530

    settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:newproject/screens/home/home.dart';
+import 'package:newproject/screens/wrapper.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
+void main() => runApp(MyApp());
 
-  ));
-}
 
-class Home extends StatelessWidget {
-  //const test({Key key}) : super(key: key);
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[900],
-      ),
-      body: Center(
-          child: /*Text(
-            'hello ninjas',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-            ),*/
-            Image(
-              image: AssetImage('assests/pic2.jpg'),
-              //image: NetworkImage('https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg'),
-          ),
-
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () { },
-        child: Text('click'),
-        backgroundColor: Colors.black12,
-      ),
-    );
-
+    return MaterialApp(
+        home: home(),
+      );
   }
 }
-
 
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:newproject/main.dart';
+
+import 'home/home.dart';
+class  Wrapper extends StatelessWidget {
+  //const ({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return home();
+  }
+}
