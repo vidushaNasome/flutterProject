91404eaf7cecf763c45b698996412573476544e9
vidushaNasome <vnasome58@gmail.com>
2021-06-10 22:08:10 +0530

    add -4

--- lib/screens/home/main.dart
+++ lib/screens/home/main.dart
@@ -6,7 +6,45 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.black45,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 200,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage('https://p.kindpng.com/picc/s/766-7660799_toyota-industries-corporation-logo-hd-png-download.png'),
+                  fit: BoxFit.cover),
+            ),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: Colors.grey.withOpacity(0.3),
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "All Moedern. All Electric.",
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 150.0),
+          Container(
+            padding: EdgeInsets.all(100),
+            /*decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage('https://p.kindpng.com/picc/s/766-7660799_toyota-industries-corporation-logo-hd-png-download.png'),
+                  fit: BoxFit.cover),
+            ),*/
 
+    )
+        ],
+      ),
     );
   }
 }
